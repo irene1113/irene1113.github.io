5894816615c568a530e688d12ed63309f988a677
IreneTung <irene277299@gmail.com>
2017-04-27 08:33:52 +0800

    remove fade effect inindex_script

--- js/index_script.js
+++ js/index_script.js
@@ -44,23 +44,23 @@ $(document).ready(function(){
 
 });
 
-$(window).on("load",function() {
-  $(window).scroll(function() {
-    $(".fade").each(function() {
-      /* Check the location of each desired element */
-      var objectCenter = $(this).offset().top + $(this).outerHeight()/2;
-      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
-
-      /* If the element is completely within bounds of the window, fade it in */
-      if (objectCenter < windowBottom) { //object comes into view (scrolling down)
-        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-      }
-      // else { //object goes out of view (scrolling up)
-      //   if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-      // }
-    });
-  });
-});
+// $(window).on("load",function() {
+//   $(window).scroll(function() {
+//     $(".fade").each(function() {
+//       /* Check the location of each desired element */
+//       var objectCenter = $(this).offset().top + $(this).outerHeight()/2;
+//       var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+//
+//       /* If the element is completely within bounds of the window, fade it in */
+//       if (objectCenter < windowBottom) { //object comes into view (scrolling down)
+//         if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+//       }
+//       // else { //object goes out of view (scrolling up)
+//       //   if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+//       // }
+//     });
+//   });
+// });
 
 function IntoProjectAnimate(element) {
   var parentSection = element.parentElement;
